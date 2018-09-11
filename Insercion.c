ef5bd6df5bf874563755ccebd062b31de0d38ad6
Galilea América Loretto Estrada <Bucketh@users.noreply.github.com>
2018-09-10 22:18:11 -0500

    Update Insercion.c

--- Insercion.c
+++ Insercion.c
@@ -1,38 +1,38 @@
+/************ INSERCION ***********
+ PÉREZ GARCÍA ATZIRI
+ LORETTO ESTRADA GALILEA AMÉRICA
+*/
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(){
-	int *Arreglo;
-	int tam, i, j, auxiliar;
+	int *Arreglo; // Se declara el contenedor de elemento.
+	int n, i, j, auxiliar; // Se declaran variables auxiliares para realizar el cambio.
 	
-	printf("Ingrese el tamanio: ");
-	scanf("%d", &tam);
+	// printf("Ingrese el tamanio: ");
+	scanf("%d", &n);
 	
-	Arreglo = (int*)malloc(sizeof(int)*tam);
+	Arreglo = (int*)malloc(sizeof(int)*n); // Reservando el espacio de memoria para el prog
 	
-	printf("Ingrese los elementos:\n");
-	for(i=0; i<tam; i++){
+	// printf("Ingrese los elementos:\n");
+	for(i=0; i<n; i++){
 		scanf("%d", &Arreglo[i]);
 	}
 	
-	printf("Los elementos del arreglo son:\n");
-	for(j=0; j<tam; j++)
-		printf("%d \n", Arreglo[j]);
-	
 	// Ordenamiento insercion
-	for(i=0; i<tam ; i++){
-		j = i;
-		auxiliar = Arreglo[i];
+	for(i=0; i<n ; i++){ // Ordenar los n elementos
+		j = i; // los iteradores i y j se igualan
+		auxiliar = Arreglo[i]; // Auxiliar toma el valor de la posicion i del arreglo
 		while((j>0)&&(auxiliar<Arreglo[j-1])){
-			Arreglo[j] = Arreglo[j-1];
-			j--;
+			Arreglo[j] = Arreglo[j-1]; // Se le asigna el  valor de la posicion anterior
+			j--; // Disminuye el iterador j.
 		}
-		Arreglo[j] = auxiliar;
+		Arreglo[j] = auxiliar; // se asigna el valor de la posicion j del arreglo a auxiliar
 	}
 	
 	// Imprimiendo arreglos
-	printf("Elementos ordenados: \n");
-	for(i=0; i<tam; i++)
+	// printf("Elementos ordenados: \n");
+	for(i=0; i<n; i++)
 		printf("%i\n", Arreglo[i]);
 	
 	return 0;
