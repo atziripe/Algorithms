8ed3e402bd0d2bf72243203a1a2d875da2c8591e
Galilea América Loretto Estrada <Bucketh@users.noreply.github.com>
2018-09-18 21:40:54 -0500

    Update sequentialSearch.c
    Aún no tiene entrada por archivo

--- Practica2/sequentialSearch.c
+++ Practica2/sequentialSearch.c
@@ -1,6 +1,27 @@
+/*****************BUSQUEDA SECUENCIAL**************************
+LORETTO ESTRADA GALILEA AMERICA
+PEREZ GARCIA ATZIRI***/
+
+/***********LIBRERIAS********************/
 #include <stdio.h>  
 #include <stdlib.h>
 
+/***********FUNCION BUSQUEDA SECUANCIAL*************/
+/* La funcion recibe como parametros el arreglo, el tamaño de este y el dato a buscar.
+ Regresa la posicion donde se encuentra el dato, o en caso contrario si este no se encuentra. */
+void sequentialSearch(int *A, int n, int buscar){
+	int j;
+	
+	for(j=0; j<n; j++){
+		if(buscar == *(A+j)){
+			printf("El elemento esta en la posicion: %d \n", j+1);
+		}
+	}
+	
+}
+
+/***********FUNCION PRINCIPAL***************/
+/* Toma como entrada la longitud del arreglo y el elemento a buscar. */
 int main(){ 
 	int *Arreglo; // Se declara el contenedor de elemento.
 	int tam, i, j; // Se declaran variables auxiliares para realizar el cambio.
@@ -19,11 +40,7 @@ int main(){
 		scanf("%d", &Arreglo[i]);
 	}
 	
-	for(j=0; j<tam; j++){
-		if(busq == Arreglo[j]){
-			printf("El elemento esta en la posicion: %d \n", j+1);
-		}
-	}
+	sequentialSearch(Arreglo,tam,busq);
 	
 	return 0;
 }
